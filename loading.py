4ddaf2e979590fb77867fe08f77389f3d183f053
aziz <mohamedaziz.mansour@polytechnicien.tn>
2024-01-02 16:23:21 +0100

    Tweak #2: Traffic Sign Recognition loading.py - documentation update

--- loading.py
+++ loading.py
@@ -179,3 +179,5 @@ def load_dataset(train_path, valid_path, valid_csv, dimensions, preloaded=False)
 # Enhancement update for 2024-09-15 12:00:00
 
 # Enhancement update for 2024-11-25 12:00:00
+
+# Enhancement update at 2024-01-02 16:23:21
