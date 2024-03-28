9de744a3626b8933dbd636468630361d54e59794
aziz <mohamedaziz.mansour@polytechnicien.tn>
2024-03-28 12:00:00 +0100

    Update #112: Traffic Sign Recognition enhancement on imagetest.py

--- imagetest.py
+++ imagetest.py
@@ -112,3 +112,5 @@ start_image_inference(image_path, yolo_model_path, cnn_model_path)
 # Enhancement update for 2024-07-16 12:00:00
 
 # Enhancement update for 2024-10-04 12:00:00
+
+# Enhancement update for 2024-03-28 12:00:00
