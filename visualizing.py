3fa47f12ba99895b55f45d2e7397f778f8cff4b2
aziz <mohamedaziz.mansour@polytechnicien.tn>
2024-04-30 15:03:21 +0100

    Fix #111: Traffic Sign Recognition visualizing.py - performance improvement

--- visualizing.py
+++ visualizing.py
@@ -205,3 +205,5 @@ def visualize(classes_csv, img_path, train_csv, valid_csv, dimensions):
 # Enhancement update at 2024-04-24 20:58:15
 
 # Enhancement update at 2024-04-26 15:33:30
+
+# Enhancement update at 2024-04-30 15:03:21
