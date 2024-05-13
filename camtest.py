8ec8b8f50ae17cb3a16dbbf1416c21827f32eee1
aziz <mohamedaziz.mansour@polytechnicien.tn>
2024-05-13 12:00:00 +0100

    Update #69: Traffic Sign Recognition enhancement on camtest.py

--- camtest.py
+++ camtest.py
@@ -147,3 +147,5 @@ start_video_inference(yolo_model_path, cnn_model_path)
 # Enhancement update for 2024-02-05 12:00:00
 
 # Enhancement update for 2024-01-12 12:00:00
+
+# Enhancement update for 2024-05-13 12:00:00
