cce6ebe97e55b966ab2e629bda00b2ca6a60224b
aziz <mohamedaziz.mansour@polytechnicien.tn>
2024-05-15 10:03:29 +0100

    Update #126: Traffic Sign Recognition training.py

--- training.py
+++ training.py
@@ -143,3 +143,5 @@ def train(dimensions, X_train, Y_train, X_validation, Y_validation, epochs, batc
 # Enhancement update at 2024-04-05 21:26:10
 
 # Enhancement update at 2024-04-07 14:59:29
+
+# Enhancement update at 2024-05-15 10:03:29
