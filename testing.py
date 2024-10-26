b4bbfdc602b7bf5206e0b38d02f413c35d98e40e
aziz <mohamedaziz.mansour@polytechnicien.tn>
2024-10-26 12:00:00 +0100

    Update #26: Traffic Sign Recognition enhancement on testing.py

--- testing.py
+++ testing.py
@@ -156,3 +156,5 @@ def start_webcam_inference(model_name, condition_prerecorded, camgui=None):
 # Enhancement update for 2024-12-20 12:00:00
 
 # Enhancement update for 2024-01-22 12:00:00
+
+# Enhancement update for 2024-10-26 12:00:00
