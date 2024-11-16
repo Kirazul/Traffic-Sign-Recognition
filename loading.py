30267da618c4b34de34e07988bc027e471cc4bce
aziz <mohamedaziz.mansour@polytechnicien.tn>
2024-11-16 12:00:00 +0100

    Update #156: Traffic Sign Recognition enhancement on loading.py

--- loading.py
+++ loading.py
@@ -173,3 +173,5 @@ def load_dataset(train_path, valid_path, valid_csv, dimensions, preloaded=False)
 # Enhancement update for 2024-05-24 12:00:00
 
 # Enhancement update for 2024-02-06 12:00:00
+
+# Enhancement update for 2024-11-16 12:00:00
