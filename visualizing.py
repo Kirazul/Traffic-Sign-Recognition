1c20dd66b9e917d040f946cdc1cbb52b043b8792
aziz <mohamedaziz.mansour@polytechnicien.tn>
2025-01-14 11:26:15 +0100

    Improve visualization of model predictions

--- visualizing.py
+++ visualizing.py
@@ -215,3 +215,5 @@ def visualize(classes_csv, img_path, train_csv, valid_csv, dimensions):
 # Enhancement update at 2024-08-08 17:26:50
 
 # Update for 2025-01-04 12:09:54: Implement interactive visualization dashboard
+
+# Update for 2025-01-14 11:26:15: Improve visualization of model predictions
